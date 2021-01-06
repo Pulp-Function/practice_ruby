68613aeb541597564903217aa89017fc06ce5978
Peter Jang <peter.x.jang@gmail.com>
2021-01-05 19:18:54 -0600

    Add employee representations using arrays/hashes/class instances

---
+++ employee.rb
@@ -0,0 +1,70 @@
+# # Using arrays
+# employee1 = ["Majora", "Carter", 80000, true]
+# employee2 = ["Danilo", "Campos", 70000, true]
+
+# puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
+# puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
+
+# # Using hashes
+# employee1 = { "first_name" => "Majora", "last_name" => "Carter", "salary" => 80000, "active" => true }
+# employee2 = { "first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true }
+
+# puts "#{employee1["first_name"]} #{employee1["last_name"]} makes #{employee1["salary"]} a year."
+# puts "#{employee2["first_name"]} #{employee2["last_name"]} makes #{employee2["salary"]} a year."
+
+# # Using hashes with symbols
+# employee1 = { :first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true }
+# employee2 = { first_name: "Danilo", last_name: "Campos", salary: 70000, active: true }
+
+# puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
+# puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
+
+# Using class instances
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+  end
+
+  def print_info()
+    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
+  end
+
+  def give_annual_raise()
+    @salary = 1.05 * @salary
+  end
+
+  def first_name()
+    @first_name
+  end
+
+  def last_name()
+    @last_name
+  end
+
+  def active()
+    @active
+  end
+
+  def first_name=(input_first_name)
+    @first_name = input_first_name
+  end
+end
+
+employee1 = Employee.new("Majora", "Carter", 80000, true)
+employee2 = Employee.new("Danilo", "Campos", 70000, true)
+employee1.print_info()
+employee2.print_info()
+
+employee2.give_annual_raise()
+
+employee2.print_info()
+
+puts employee1.first_name()
+puts employee1.last_name()
+puts employee1.active()
+
+employee1.first_name = "Bob"
+puts employee1.first_name
